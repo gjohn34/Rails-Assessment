7eb73516b1f245f9f1d0e911f2648f903961aedf
Aaron <gitkraken@aaronlcjones.com>
2019-05-03 13:24:33 +1000

    Fixed carousel + materialbox bug
    Event listener might slow things down - if performance is slow will have to remove

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-        var elems = document.querySelectorAll('.carousel');
-        var instances = M.Carousel.init(elems, {
-            dist: -50})
-            // onCycleTo: function(data){
-            //     console.log(data);
-            // }});
-    }); 
-
-
-
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        dist: -50
+    });
+});
