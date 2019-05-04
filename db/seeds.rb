ebed0bbcc53b82177f1e5c29902ff59d722b399d
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-05-04 21:59:05 +1000

    added users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([{
+  email: "test1@test.com",
+  password: "password",
+  password_confirmation: "password"
+},
+{
+  email: "test2@test.com",
+  password: "password",
+  password_confirmation: "password"
+},
+{
+  email: "test3@test.com",
+  password: "password",
+  password_confirmation: "password"
+},
+{
+  email: "test4@test.com",
+  password: "password",
+  password_confirmation: "password"
+  }])
 
 Interest.create!([{
   interest: "Cats"
@@ -19,21 +39,27 @@ Interest.create!([{
   interest: "Fridges"
   }])
 
+
+
+
 [
   {
     name: "John Smith",
-    bio: "I am John Smith"
+    bio: "I am John Smith",
+    user_id: 1
   },
   {
     name: "Alex Jones",
-    bio: "They're turning the freaking frogs gay"
+    bio: "They're turning the freaking frogs gay",
+    user_id: 2
   },
   {
     name: "Tom Green",
-    bio: "I am Tom Green"
+    bio: "I am Tom Green",
+    user_id: 3
   }
 ].each do |profile|
-  resource = Profile.new(name: profile[:name], bio: profile[:bio])
+  resource = Profile.new(name: profile[:name], bio: profile[:bio], user_id: profile[:user_id])
   resource.pic.attach(io: File.open(Rails.root.join("app", "assets", "images", "anon.png")), filename: 'anon.png', content_type: "image/png")
   resource.save
 end
@@ -42,3 +68,7 @@ profile = Profile.all
 profile.each do |profile|
   Like.create!(profile_id: profile.id)
 end
+
+puts User.all.count
+puts Interest.all.count
+puts Profile.all.count
