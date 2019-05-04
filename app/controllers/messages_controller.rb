6090f935ffe4801add5a7487aa433b5c2757c50d
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-05-04 17:59:52 +1000

    fix mail index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 
   def index
     #have to change below to current user
-    # @profile = Profile.friendly.find(4)
+    # @profile = Profile.friendly.find(2) #currently AJ
     # @messages = Message.where(profile_id: @profile.id)
   end
 
