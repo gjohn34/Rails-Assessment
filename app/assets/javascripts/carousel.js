46ceb0e5e9010e94df2038f2e43c5d7bd2e14788
Aaron <gitkraken@aaronlcjones.com>
2019-05-10 12:14:53 +1000

    Css for carousel

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
-        dist: -50
+        dist: -100,
+        padding: 50,
+        numVisible: 7,
+        duration: 50
     });
 });
